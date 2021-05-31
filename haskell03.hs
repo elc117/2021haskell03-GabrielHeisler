b06e690c7f6be558747a3601ab720cddda74c593
Gabriel Heisler <gvheisler@inf.ufsm.br>
2021-05-31 16:33:05 -0300

    update

--- haskell03.hs
+++ haskell03.hs
@@ -20,11 +20,11 @@ addSuffix str strl = [x ++ str | x <- strl]
 selectgt5 :: [Int] -> [Int]
 selectgt5 list = [x | x <- list, x>5]
 
---sumOdds :: [Int] -> Int
---sumOdds list = [x | x <- list, odd x]
+sumOdds :: [Int] -> Int
+sumOdds list = sum [x | x <- list, odd x]
 
---sumOdds' :: [Int] -> Int
---sumOdds' list = 
+sumOdds' :: [Int] -> Int
+sumOdds' list = sum(filter odd list)
 
 selectExpr :: [Int] -> [Int]
 selectExpr list = [x | x <- list, x>=20 && x<=50 && even x]
@@ -35,6 +35,8 @@ countShorts list = length ([x | x <- list, length x < 5])
 calcExpr :: [Float] -> [Float]
 calcExpr list = [x^2/2 | x <- list, x^2/2 > 10]
 
---trSpaces :: String -> String
---trSpaces str = [x = '-' | x <- str, x==' ']
+trSpaces :: String -> String
+trSpaces str = [if x == ' ' then '-' else x | x <- str]
+
+
 
