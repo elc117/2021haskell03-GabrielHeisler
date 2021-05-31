79b31b669792ca0fae209a70c493308fea170b66
Gabriel Heisler <gvheisler@inf.ufsm.br>
2021-05-31 15:52:02 -0300

    functions added

--- haskell03.hs
+++ haskell03.hs
@@ -14,3 +14,27 @@ applyExpr list = [3*x+2 | x <- list]
 applyExpr' :: [Int] -> [Int]
 applyExpr' list = map (\x -> 3*x+2) list
 
+addSuffix :: String -> [String] -> [String]
+addSuffix str strl = [x ++ str | x <- strl]
+
+selectgt5 :: [Int] -> [Int]
+selectgt5 list = [x | x <- list, x>5]
+
+--sumOdds :: [Int] -> Int
+--sumOdds list = [x | x <- list, odd x]
+
+--sumOdds' :: [Int] -> Int
+--sumOdds' list = 
+
+selectExpr :: [Int] -> [Int]
+selectExpr list = [x | x <- list, x>=20 && x<=50 && even x]
+
+countShorts :: [String] -> Int
+countShorts list = length ([x | x <- list, length x < 5])
+
+calcExpr :: [Float] -> [Float]
+calcExpr list = [x^2/2 | x <- list, x^2/2 > 10]
+
+--trSpaces :: String -> String
+--trSpaces str = [x = '-' | x <- str, x==' ']
+
